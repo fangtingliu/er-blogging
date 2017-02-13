5abb9723cd11f5fa04e163ead8de6c22b94b725c
Fangting <fangtingprahl@gmail.com>
2017-02-13 17:53:33 -0500

    file-uploader

--- app/components/file-uploader.js
+++ app/components/file-uploader.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  uploading: false,
+  didInsertElement: function() {
+    this.$('#fileupload').fileupload({
+      url: '/fakeurl.com',
+      method: 'post',
+      add (e, data) {
+        component.set("uploading",true);
+        console.log('data in fileupload: ', data);
+      }
+    });
+  }
 });
