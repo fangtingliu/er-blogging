76499c91bfd8f02faef9b4a4b0876dca6cdf4d39
fangting <fangtingprahl@gmail.com>
2017-02-22 16:42:04 -0500

    clear fields

--- app/controllers/posts/show.js
+++ app/controllers/posts/show.js
@@ -3,15 +3,21 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     submitComment() {
+      const controller = this;
       const input = {
-        postId: this.get('model.id'),
-        name: this.get('name'),
-        email: this.get('email'),
-        comment: this.get('comment')
+        postId: controller.get('model.id'),
+        name: controller.get('name'),
+        email: controller.get('email'),
+        comment: controller.get('comment')
       };
       console.log('show Controller input: ', input)
-      const comment = this.get('store').createRecord('comment', input);
-      comment.save();
+      const comment = controller.get('store').createRecord('comment', input);
+      comment.save().then(function(){
+        console.log('succeed submitComment')
+        controller.set('name', '');
+        controller.set('email', '');
+        controller.set('comment', '');
+      });
     }
   }
 });
