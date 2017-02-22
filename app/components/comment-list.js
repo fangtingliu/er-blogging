0a62e4bd99c719efec2d7cb775e204cc2aeefa87
fangting <fangtingprahl@gmail.com>
2017-02-22 18:16:47 -0500

    get comments in bulk

--- app/components/comment-list.js
+++ app/components/comment-list.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   reversedComments: function() {
     return this.get('comments').toArray().reverse();
-  }.property('model.@each')
+  }.property('comments.[]')
 });
