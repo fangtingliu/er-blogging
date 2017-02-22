5fda18c291575a33087144c8c0f4f0dd39a629ee
fangting <fangtingprahl@gmail.com>
2017-02-22 10:28:00 -0500

    minor post style fix and code clean

--- app/helpers/post-content-formatter.js
+++ app/helpers/post-content-formatter.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 
 export function postContentFormatter([content]/*, hash*/) {
-  let formatted_content = content.split('\n');
+  if (/\<p\>/.test(content)) {
+    return content;
+  }
+  let formatted_content = content.split('\n ');
   console.log('content in postContentFormatter: ', formatted_content);
   return Ember.String.htmlSafe('<p>' + formatted_content.join("</p><p>"));
 }
