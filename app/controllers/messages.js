c2fc26683972d369d688ccb024b1b7d29fe01b18
fangting <fangtingprahl@gmail.com>
2017-02-23 18:44:52 -0500

    add messages query tabs

---
+++ app/controllers/messages.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  read: 'false',
+  queryParams: ['read']
+});
--- app/routes/messages.js
+++ app/routes/messages.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.get('store').findAll('message');
+  queryParams: {
+    read: {
+      refreshModel: true
+    }
+  },
+  model(params) {
+    console.log('in model: params : ', params)
+    if (params.read === 'all') {
+      return this.get('store').findAll('message');
+    } else {
+      return this.get('store').query('message', params);
+    }
   },
   actions: {
     readMessage(message) {
@@ -10,13 +20,23 @@ export default Ember.Route.extend({
       console.log('readMessage message: ', message);
       message.set('read', true);
       message.save();
-      // route.controller.get('model').reload().then(function(model) {
-      //   console.log('in message Route reload cb: ', model)
-      // });
-      // route.modelFor('message').reload();
-      // const model = route.controller.get('model');
-      // console.log('readMessage model: ', model);
-      // model.set({'read': true});
+      // TODO: add refreshModel after save
+    },
+    getMessageWithQuery(read) {
+      // TODO: clean code and add tab active func into tabs
+      const controller = this.controller;
+      var queryParams = controller.get('queryParams');
+      console.log('read in getMessageWithQuery: ', read, read === 'all', event)
+      const m$ =  $(`#${this.get('elementId')}`);
+      console.log('get element: ',m$.find('#all'))
+      if (read === 'all') {
+        controller.set('read', 'all');
+        m$.find('#all').addClass('active');
+        console.log('after set read to all: ', queryParams, controller.get('read'))
+      } else {
+        controller.set('read', read);
+      }
+      // console.log('read in getMessageWithQuery: ', read, queryParams);
     }
   }
 });
